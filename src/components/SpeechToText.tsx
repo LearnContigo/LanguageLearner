f1bbba607b9be68e255fbfedcc4a56761c77f7f4
Justin Kroeger <jkroeger75@gmail.com>
2022-06-24 18:34:17 -0400

    speech

--- src/components/SpeechToText.tsx
+++ src/components/SpeechToText.tsx
@@ -1,6 +1,6 @@
 import React, { useState} from 'react'
 import { getTokenOrRefresh } from '../util/tokenUtil'
-import { Recognizer, SpeechRecognitionEventArgs } from 'microsoft-cognitiveservices-speech-sdk'
+import { Recognizer, SpeechRecognitionEventArgs, SpeechSynthesisResult } from 'microsoft-cognitiveservices-speech-sdk'
 
 const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
 
@@ -8,6 +8,22 @@ const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
 const SpeechToText: React.FC = () => {
 
     const [displayText, setDisplayText] = useState("");
+
+    const OutputSpeech = async (text: string) => {
+        const tokenObj = await getTokenOrRefresh();
+
+        if(!tokenObj.authToken)
+            return setDisplayText("Error Fetching Token");
+
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        const audioConfig = speechsdk.AudioConfig.fromDefaultSpeakerOutput();
+        speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
+        const synthesizer = new speechsdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+        synthesizer.speakTextAsync(text, (e: SpeechSynthesisResult) => {
+            synthesizer.close();
+        });
+    }
 
     const TranscribeMic = async () => {
         const tokenObj = await getTokenOrRefresh();
@@ -17,12 +33,13 @@ const SpeechToText: React.FC = () => {
 
         const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
         speechConfig.speechRecognitionLanguage = 'en-US';
-            
+        
         const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
         const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
 
         recognizer.recognized = (sender: Recognizer, event: SpeechRecognitionEventArgs) => {
             setDisplayText(event.result.text);
+            OutputSpeech(event.result.text);
         }
 
         recognizer.startContinuousRecognitionAsync();
